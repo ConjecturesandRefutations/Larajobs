f498564bff67747a72467f862048d6363a04e800
Alfie <alfiecollins44@gmail.com>
2023-10-26 18:17:59 +0100

    creating an eloquent model

---
+++ data.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    [
+        'title' => 'CEO',
+        'tags' => 'laravel, javascript',
+        'company' => 'AlfCo',
+        'email' => 'alfie@alfco.com',
+    ],
+    [
+        'title' => 'CEO',
+        'tags' => 'laravel, javascript',
+        'company' => 'AlfCo',
+        'email' => 'alfie@alfco.com',
+    ],
+    [
+        'title' => 'CEO',
+        'tags' => 'laravel, javascript',
+        'company' => 'AlfCo',
+        'email' => 'alfie@alfco.com',
+    ]
+    ];
+
+    ?>
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,26 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          \App\Models\User::factory(10)->create();
+
+         Listing::create([
+            'title' => 'CEO',
+            'tags' => 'laravel, javascript',
+            'company' => 'AlfCo',
+            'location' => 'Delhi',
+            'email' => 'alfie@alfco.com',
+            'website' => 'alfie.com',
+            'description' => 'lorem ipsum'
+        ]);
+
+        Listing::create([
+            'title' => 'Engineer',
+            'tags' => 'Engineering',
+            'company' => 'AlfCo',
+            'location' => 'Paris',
+            'email' => 'bob@alfco.com',
+            'website' => 'bob.com',
+            'description' => 'lorem ipsum again'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
